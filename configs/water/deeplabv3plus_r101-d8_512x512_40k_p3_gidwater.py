9b8d147ea15fe888b90c8ab02641e517221e7732
isolution911 <1643549077@qq.com>
2022-02-18 14:31:01 +0800

    r101 debug

--- configs/water/deeplabv3plus_r101-d8_512x512_40k_p3_gidwater.py
+++ configs/water/deeplabv3plus_r101-d8_512x512_40k_p3_gidwater.py
@@ -12,6 +12,8 @@ model = dict(
 dataset_type = 'WaterDataset'
 data_root = 'data/GID'
 data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         data_root=data_root,
@@ -27,5 +29,15 @@ data = dict(
         data_root=data_root,
         img_dir='img_dir/val',
         ann_dir='label2mask/val'))
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(policy='poly', power=0.9, min_lr=5e-5, by_epoch=False)
+# runtime settings
+runner = dict(type='IterBasedRunner', max_iters=80000)
+checkpoint_config = dict(by_epoch=False, interval=8000)
+evaluation = dict(interval=8000, metric='mIoU', pre_eval=True)
 
 work_dir = './work_dirs/water/deeplabv3plus_r101-d8_512x512_40k_p3_gidwater'
